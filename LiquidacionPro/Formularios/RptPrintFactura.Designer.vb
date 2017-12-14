cd130c196eb692ee007bb88d0bcd44f39a21c254
rasec <cesar_andreme@live.com>
2017-12-14 12:35:48 -0500

    se agrega impresión de factura

---
+++ LiquidacionPro/Formularios/RptPrintFactura.Designer.vb
@@ -0,0 +1,84 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class RptPrintFactura
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(RptPrintFactura))
+        Me.PrintDocument1 = New System.Drawing.Printing.PrintDocument()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.PrintDialog1 = New System.Windows.Forms.PrintDialog()
+        Me.PrintPreviewDialog1 = New System.Windows.Forms.PrintPreviewDialog()
+        Me.PrintPreviewControl1 = New System.Windows.Forms.PrintPreviewControl()
+        Me.SuspendLayout()
+        '
+        'PrintDocument1
+        '
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(2, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 0
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'PrintDialog1
+        '
+        Me.PrintDialog1.UseEXDialog = True
+        '
+        'PrintPreviewDialog1
+        '
+        Me.PrintPreviewDialog1.AutoScrollMargin = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.AutoScrollMinSize = New System.Drawing.Size(0, 0)
+        Me.PrintPreviewDialog1.ClientSize = New System.Drawing.Size(400, 300)
+        Me.PrintPreviewDialog1.Enabled = True
+        Me.PrintPreviewDialog1.Icon = CType(resources.GetObject("PrintPreviewDialog1.Icon"), System.Drawing.Icon)
+        Me.PrintPreviewDialog1.Name = "PrintPreviewDialog1"
+        Me.PrintPreviewDialog1.Visible = False
+        '
+        'PrintPreviewControl1
+        '
+        Me.PrintPreviewControl1.Location = New System.Drawing.Point(2, 32)
+        Me.PrintPreviewControl1.Name = "PrintPreviewControl1"
+        Me.PrintPreviewControl1.Size = New System.Drawing.Size(779, 297)
+        Me.PrintPreviewControl1.TabIndex = 1
+        '
+        'RptPrintFactura
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(793, 341)
+        Me.Controls.Add(Me.PrintPreviewControl1)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "RptPrintFactura"
+        Me.Text = "RptPrintFactura"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PrintDocument1 As Printing.PrintDocument
+    Friend WithEvents Button1 As Button
+    Friend WithEvents PrintDialog1 As PrintDialog
+    Friend WithEvents PrintPreviewDialog1 As PrintPreviewDialog
+    Friend WithEvents PrintPreviewControl1 As PrintPreviewControl
+End Class
---
+++ LiquidacionPro/Formularios/formatNUMBER.vb
@@ -0,0 +1,239 @@
+﻿Public Class formatNUMBER
+    Public Function DecimalLetras(ByVal numero As String) As String
+        '********Declara variables de tipo cadena************
+        Dim palabras, entero, dec, flag As String
+        palabras = String.Empty
+        dec = String.Empty
+        entero = String.Empty
+        flag = String.Empty
+        '********Declara variables de tipo entero***********
+        Dim num, x, y As Integer
+
+        flag = "N"
+
+        '**********Número Negativo***********
+        If Mid(numero, 1, 1) = "-" Then
+            numero = Mid(numero, 2, numero.ToString.Length - 1).ToString
+            palabras = "MENOS "
+        End If
+
+        '**********Si tiene ceros a la izquierda*************
+        For x = 1 To numero.ToString.Length
+            If Mid(numero, 1, 1) = "0" Then
+                numero = Trim(Mid(numero, 2, numero.ToString.Length).ToString)
+                If numero.ToString.Trim.Length = 0 Then
+                    palabras = String.Empty
+                End If
+            Else
+                Exit For
+            End If
+        Next
+
+        '*********Dividir parte entera y decimal************
+        For y = 1 To Len(numero)
+            If Mid(numero, y, 1) = "." Then
+                flag = "S"
+            Else
+                If flag = "N" Then
+                    entero = entero + Mid(numero, y, 1)
+                Else
+                    dec = dec + Mid(numero, y, 1)
+                End If
+            End If
+        Next y
+
+        If Len(dec) = 1 Then dec = dec & "0"
+
+        '**********proceso de conversión***********
+        flag = "N"
+
+        If Val(numero) <= 999999999 Then
+            For y = Len(entero) To 1 Step -1
+                num = Len(entero) - (y - 1)
+                Select Case y
+                    Case 3, 6, 9
+                        '**********Asigna las palabras para las centenas***********
+                        Select Case Mid(entero, num, 1)
+                            Case "1"
+                                If Mid(entero, num + 1, 1) = "0" And Mid(entero, num + 2, 1) = "0" Then
+                                    palabras = palabras & "CIEN "
+                                Else
+                                    palabras = palabras & "CIENTO "
+                                End If
+                            Case "2"
+                                palabras = palabras & "DOCIENTOS "
+                            Case "3"
+                                palabras = palabras & "TRESCIENTOS "
+                            Case "4"
+                                palabras = palabras & "CUATROCIENTOS "
+                            Case "5"
+                                palabras = palabras & "QUINIENTOS "
+                            Case "6"
+                                palabras = palabras & "SEISCIENTOS "
+                            Case "7"
+                                palabras = palabras & "SETECIENTOS "
+                            Case "8"
+                                palabras = palabras & "OCHOCIENTOS "
+                            Case "9"
+                                palabras = palabras & "NOVECIENTOS "
+                        End Select
+                    Case 2, 5, 8
+                        '*********Asigna las palabras para las decenas************
+                        Select Case Mid(entero, num, 1)
+                            Case "1"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    flag = "S"
+                                    palabras = palabras & "DIEZ "
+                                End If
+                                If Mid(entero, num + 1, 1) = "1" Then
+                                    flag = "S"
+                                    palabras = palabras & "ONCE "
+                                End If
+                                If Mid(entero, num + 1, 1) = "2" Then
+                                    flag = "S"
+                                    palabras = palabras & "DOCE "
+                                End If
+                                If Mid(entero, num + 1, 1) = "3" Then
+                                    flag = "S"
+                                    palabras = palabras & "TRECE "
+                                End If
+                                If Mid(entero, num + 1, 1) = "4" Then
+                                    flag = "S"
+                                    palabras = palabras & "CATORCE "
+                                End If
+                                If Mid(entero, num + 1, 1) = "5" Then
+                                    flag = "S"
+                                    palabras = palabras & "QUINCE "
+                                End If
+                                If Mid(entero, num + 1, 1) > "5" Then
+                                    flag = "N"
+                                    palabras = palabras & "DIECI"
+                                End If
+                            Case "2"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "VEINTE "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "VEINTI"
+                                    flag = "N"
+                                End If
+                            Case "3"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "TREINTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "TREINTA Y "
+                                    flag = "N"
+                                End If
+                            Case "4"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "CUARENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "CUARENTA Y "
+                                    flag = "N"
+                                End If
+                            Case "5"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "CINCUENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "CINCUENTA Y "
+                                    flag = "N"
+                                End If
+                            Case "6"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "SESENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "SESENTA Y "
+                                    flag = "N"
+                                End If
+                            Case "7"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "SETENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "SETENTA Y "
+                                    flag = "N"
+                                End If
+                            Case "8"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "OCHENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "OCHENTA Y "
+                                    flag = "N"
+                                End If
+                            Case "9"
+                                If Mid(entero, num + 1, 1) = "0" Then
+                                    palabras = palabras & "NOVENTA "
+                                    flag = "S"
+                                Else
+                                    palabras = palabras & "NOVENTA Y "
+                                    flag = "N"
+                                End If
+                        End Select
+                    Case 1, 4, 7
+                        '*********Asigna las palabras para las unidades*********
+                        Select Case Mid(entero, num, 1)
+                            Case "1"
+                                If flag = "N" Then
+                                    If y = 1 Then
+                                        palabras = palabras & "UNO "
+                                    Else
+                                        palabras = palabras & "UN "
+                                    End If
+                                End If
+                            Case "2"
+                                If flag = "N" Then palabras = palabras & "DOS "
+                            Case "3"
+                                If flag = "N" Then palabras = palabras & "TRES "
+                            Case "4"
+                                If flag = "N" Then palabras = palabras & "CUATRO "
+                            Case "5"
+                                If flag = "N" Then palabras = palabras & "CINCO "
+                            Case "6"
+                                If flag = "N" Then palabras = palabras & "SEIS "
+                            Case "7"
+                                If flag = "N" Then palabras = palabras & "SIETE "
+                            Case "8"
+                                If flag = "N" Then palabras = palabras & "OCHO "
+                            Case "9"
+                                If flag = "N" Then palabras = palabras & "NUEVE "
+                        End Select
+                End Select
+
+                '***********Asigna la palabra mil***************
+                If y = 4 Then
+                    If Mid(entero, 6, 1) <> "0" Or Mid(entero, 5, 1) <> "0" Or Mid(entero, 4, 1) <> "0" Or
+                        (Mid(entero, 6, 1) = "0" And Mid(entero, 5, 1) = "0" And Mid(entero, 4, 1) = "0" And
+                        Len(entero) <= 6) Then palabras = palabras & "MIL "
+                End If
+
+                '**********Asigna la palabra millón*************
+                If y = 7 Then
+                    If Len(entero) = 7 And Mid(entero, 1, 1) = "1" Then
+                        palabras = palabras & "MILLÓN "
+                    Else
+                        palabras = palabras & "MILLONES "
+                    End If
+                End If
+            Next y
+
+            '**********Une la parte entera y la parte decimal*************
+            If dec <> String.Empty Then
+                If (dec = 0) Then
+                    DecimalLetras = palabras & "CON " & "00" & "/100 "
+                Else
+                    DecimalLetras = palabras & "CON " & CStr(CInt(dec)) & "/100 "
+                End If
+
+            Else
+                DecimalLetras = palabras
+            End If
+        Else
+            DecimalLetras = String.Empty
+        End If
+    End Function
+End Class
